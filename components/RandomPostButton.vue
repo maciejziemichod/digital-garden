c6316795cbe3a37a404a462bdd43c58d5da40b4a
maciejziemichod <maciej.ziemichod@gmail.com>
2020-12-26 22:42:48 +0100

    feat: created random post button, not used yet

---
+++ components/RandomPostButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <nuxt-link :to="link" @click.native="randomLink">random post</nuxt-link>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      link: "",
+    };
+  },
+  methods: {
+    async randomLink() {
+      const fetchedArticles = await this.$content().only(["path"]).fetch();
+      const randomIndex = Math.floor(Math.random() * fetchedArticles.length);
+      this.link = fetchedArticles[randomIndex].path;
+    },
+  },
+};
+</script>
